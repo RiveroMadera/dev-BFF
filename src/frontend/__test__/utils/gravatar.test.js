3e659f61dd9c7a6078591111083a94a8f9e64273
RiveroMadera <jesusrivero19@gmail.com>
2021-07-12 00:05:39 -0400

    added test to gravatar and the actions

---
+++ src/frontend/__test__/utils/gravatar.test.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+import gravatar from '../../utils/gravatar';
+
+test('Gravatar Function Test', () => {
+  const email: 'jesusrivero19@gmail.com';
+  const gravatarUrl = 'https://gravatar.com/avatar/d55c814ece902dca4708f5e6ae7a0991?s=80';
+  expect(gravatarUrl).toEqual(gravatar(email));
+});
